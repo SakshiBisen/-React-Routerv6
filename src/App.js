de0bda095c07e7c52ef5208f1719dc51715635cc
SakshiBisen <sakshibisen796@gmail.com>
2024-08-21 17:04:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+ 
+function App() {
+  return (
+    <div className="App">
+       
+       <BrowserRouter>
+       <Navbar/>
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+       </Routes>
+       </BrowserRouter>
+        
+    </div>
+  );
+}
+ 
+export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+ 
+const Home = () => {
+  return (
+    <>
+    <div  class="px-4 py-5 my-5 text-center">
+        <img  width="617" height="367"  viewBox='0 0 617 367' src="https://th.bing.com/th/id/OIP.HCcObDF28L8bxM4Zwsn9XAAAAA?w=350&h=450&rs=1&pid=ImgDetMain"/>
+    <svg width="60" height="30"  viewBox='0 0 617 367 fill= '></svg>    
+ 
+    <h1 class="display-5 fw-bold">Hellow People</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Getting Started</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Contact-Us</button>
+      </div>
+       
+    </div>
+  </div>
+  </>
+  )
+}
+ 
+export default Home
